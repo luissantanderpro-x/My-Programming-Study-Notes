30803c02c9946332320d2cd6f4b3ecacd8a32d7d
luissantanderdev <luissantanderpro@proton.me>
2024-05-24 22:02:30 -0700

    worked on reviewing AVL trees

---
+++ Algorithms & Data Structures Review Prep/10. AVL Trees/python_code/main.py
@@ -0,0 +1,140 @@
+# AVL Tree
+
+class Node:
+    def __init__(self, data: any, parent: 'Node' = None) -> None:
+        self.data = data
+        self.left_node = None
+        self.right_node = None
+        self.parent = parent
+        self.height = 0
+
+class AVLTree:
+    def __init__(self) -> None:
+        '''we can access the root node exclusively'''
+        self.root = None
+
+    def __insert_node(self, data: any, node: Node):
+        '''inserts the data to either left or right side.'''
+        # we have to consider the left subtree 
+        if (data < node.data):
+            # we have to check if the left node is a None
+            # when the left child is not a None 
+            if (node.left_node):
+                self.__insert_node(data, node.left_node)
+            else:
+                node.left_node = Node(data, node) 
+                node.height = max(self.__calc_height(node.left_node), self.__calc_height(node.right_node)) + 1
+        else:
+            # we have to check if the right node is a None
+            # when the right child is not None 
+            if (node.right_node):
+                self.__insert_node(data, node.right_node)
+            else:
+                node.right_node = Node(data, node) 
+                node.height = max(self.__calc_height(node.left_node), self.__calc_height(node.right_node)) + 1
+
+    def __remove_node(self, data: any, node: Node):
+        if (node is None):
+            return
+        
+        if (data < node.data):
+            self.__remove_node(data, node.left_node)
+        elif (data > node.data):
+            self.__remove_node(data, node.right_node) 
+        else:
+            # we have found the node we want to remove. 
+            # case #1 if the node is a leaf node 
+            if (node.left_node is None and node.right_node is None):
+                print('Removing a leaf node...%d' % node.data)
+                parent = node.parent
+
+                if (parent is not None and parent.left_node == node):
+                    parent.left_node - None 
+                if (parent is not None and parent.right_node == node):
+                    parent.right_node = None 
+
+                if (parent is None):
+                    self.root = None 
+                
+                del node 
+
+                # after every insertion WE HAVE TO CHECK whether the AVL properties are violated.
+                self.__handle_violations(node) 
+            
+            # case 2:) if the node has a single child.
+            elif (node.left_node is None and node.right_node is not None):
+                print('Removing a node with single right child...') 
+                parent = node.parent
+
+                if (parent is not None):
+                    if (parent.left_node == node):
+                        parent.left_node = node.right_node
+                    if (parent.right_node == node):
+                        parent.right_node = node.right_node
+                else:
+                    self.root = node.right_node
+                
+                node.right_node.parent = parent 
+                del node 
+
+                self.__handle_violations(node) 
+            
+            elif (node.right_node is None and node.left_node is not None):
+                print('Removing a node with single left child...') 
+                parent = node.parent
+
+                if (parent is not None):
+                    if (parent.left_node == node):
+                        parent.left_node = node.left_node
+                    if (parent.right_node == node):
+                        parent.right_node = node.left_node
+                else:
+                    self.root = node.left_node
+                
+                node.left_node.parent = parent 
+                del node 
+
+                self.__handle_violations(node) 
+            
+            # case 3 the node has 2 children 
+            else: 
+                print('Removing node with two children.....') 
+                predecessor = self.__get_predecessor(node.left_node) 
+
+                temp = predecessor.data 
+                predecessor.data = node.data 
+                node.data = temp 
+
+                self.__remove_node(data, predecessor) 
+
+    def __get_predecessor(self, node: Node):
+        if (node.right_node):
+            return self.__get_predecessor(node.right_node)
+        return node 
+
+
+    def __handle_violations(self, node: Node):
+        '''checks if AVL tree properties have been violated or not.'''
+        pass 
+
+
+    def remove(self, data: any):
+        '''removes a node from the AVL Tree'''
+        if (self.root):
+            self.__remove_node()
+
+
+
+    def insert(self, data: any) -> None: 
+        '''inserts data to the avl tree.'''
+        if (self.root is None):
+            self.root = Node(data)
+        else:
+            self.__insert_node(data, self.root) 
+        
+        # after every insertion WE HAVE TO CHECK whether the AVL properties
+        # are violated 
+        self.__handle_violations(node) 
+
+if __name__ == '__main__':
+    print(True)
